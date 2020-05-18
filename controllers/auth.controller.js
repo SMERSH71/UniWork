082273a48b4e6fd4cb0cfdcd7b194504735cd5af
Dmitry <dmitry.samson@mail.ru>
2020-05-18 12:36:28 +0300

    make it better

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -82,65 +82,6 @@ class AuthController {
       return res.send({ error: true, data: "Internal server error." })
   }
 
-  /* [+] Добавить профессию */
-  async create(req, res) {
-    if (!dataCheker.checkData("profession", req.body))
-      return res.send({ error: true, data: "Bad request." })
-
-    let result = await AuthService.create(req.body)
-    if (result)
-      return res.status(200).send({ error: false, data: result })
-    else
-      return res.send({ error: true, data: "Internal server error." })
-  }
-
-  /* [+] Изменить профессию по коду */
-  async update(req, res) {
-    let kod = req.params.kod
-
-    if (!dataCheker.checkType(+kod, "number"))
-      return res.send({ error: true, data: "Bad request." })
-
-    if (!dataCheker.checkData("profession", req.body))
-      return res.send({ error: true, data: "Bad request." })
-
-    let result = await AuthService.update(kod, req.body)
-    if (result)
-      return res.status(200).send({ error: false, data: result })
-    else
-      return res.send({ error: true, data: "Internal server error." })
-  }
-
-  /* [+] Удалить профессию по коду */
-  async remove(req, res) {
-    let kod = req.params.kod
-
-    if (!dataCheker.checkType(+kod, "number"))
-      return res.send({ error: true, data: "Bad request." })
-
-    let result = await AuthService.remove(kod)
-    if (result)
-      return res.status(200).send({ error: false, data: result })
-    else
-      return res.send({ error: true, data: "Internal server error." })
-  }
-
-  /* [+] Поиск профессии по названию */
-  async search(req, res) {
-    let search = req.query.text
-
-    if (search == undefined || search == '')
-      return res.send({ error: false, data: [] })
-
-    search = search.toLowerCase()
-
-    let result = await AuthService.search(search)
-    if (result)
-      return res.status(200).send({ error: false, data: result })
-    else
-      return res.send({ error: true, data: "Internal server error." })
-  }
-
 }
 
 module.exports = new AuthController()
